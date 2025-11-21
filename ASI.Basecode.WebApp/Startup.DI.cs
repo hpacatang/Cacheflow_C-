c5b95fc195818f69cf4daf08d57dcfa7692d095c
Kyle Bonghanoy <kylebonghanoy32@gmail.com>
2025-11-22 01:46:59 +0800

    Register FeedbackService and FeedbackRepository

--- ASI.Basecode.WebApp/Startup.DI.cs
+++ ASI.Basecode.WebApp/Startup.DI.cs
@@ -33,11 +33,14 @@ namespace ASI.Basecode.WebApp
             this._services.TryAddSingleton<TokenValidationParametersFactory>();
             this._services.AddScoped<IUserService, UserService>();
             this._services.AddScoped<ITicketService, TicketService>();
+            this._services.AddScoped<IFeedbackService, FeedbackService>();
 
 
             // Repositories
             this._services.AddScoped<IUserRepository, UserRepository>();
-
+            this._services.AddScoped<ITicketRepository, TicketRepository>();
+            this._services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+            
             // Manager Class
             this._services.AddScoped<SignInManager>();
 
